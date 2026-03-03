649f95a65291ea411309bb472ce94a5d102f4e36
Harshitha <24596.cs@rmkcet.ac.in>
2026-03-03 19:15:44 +0530

    Implement palindrome checker

---
+++ DAY 3/palindrome.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int temp = num, rev = 0;
+        while (temp != 0) {
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
+        }
+
+        if (num == rev)
+            System.out.println("True");
+        else
+            System.out.println("False");
+    }
+}
